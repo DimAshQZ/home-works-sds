846cca288ea421a8e706d444ececbb903df20593
DimAshQZ <53437421+DimAshQZ@users.noreply.github.com>
2021-11-10 19:53:21 +0600

    initial commit

---
+++ src/pro/sky/java/cource2/hw2/Bicycle.java
@@ -0,0 +1,11 @@
+package pro.sky.java.cource2.hw2;
+
+public class Bicycle {
+
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
